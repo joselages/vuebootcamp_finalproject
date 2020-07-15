1a5e0389e7d0a3e241f57abc662b02d43a2ffe88
jlages <j.lages@ydigitalmedia.com>
2020-07-15 20:20:05 +0100

    Setup project

---
+++ src/App.vue
@@ -0,0 +1,99 @@
+<template>
+  <div id="app">{{ola}}</div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      ola: "Hello World"
+    };
+  }
+};
+</script>   
+
+<style>
+body {
+  font-family: "Poppins", sans-serif;
+  color: #444;
+}
+
+h1 {
+  font-weight: 800;
+  text-align: center;
+  margin: 1.5em 0;
+  font-size: 1.6em;
+}
+
+.input-field {
+  display: flex;
+  flex-direction: column;
+  width: 20em;
+  padding-bottom: 10px;
+  border-bottom: 1px solid #ccc;
+  margin-bottom: 20px;
+}
+
+.input-field label {
+  font-size: 0.75em;
+  margin-bottom: 10px;
+}
+
+.input-field input {
+  padding: 10px 0;
+}
+
+.currency {
+  font-size: 25px;
+  padding: 2px 8px 0 0;
+  color: #999;
+}
+
+.toggle-switch {
+  display: flex;
+  margin-bottom: 36px;
+  overflow: hidden;
+}
+
+.toggle-switch input {
+  position: absolute !important;
+  clip: rect(0, 0, 0, 0);
+  height: 1px;
+  width: 1px;
+  border: 0;
+  overflow: hidden;
+}
+
+.toggle-switch label {
+  background-color: #ebf8ff;
+  color: rgba(0, 0, 0, 0.6);
+  font-size: 14px;
+  line-height: 1;
+  text-align: center;
+  padding: 1.2em 1.6em;
+  border: 1px solid rgba(0, 0, 0, 0.2);
+  box-shadow: inset 0 1px 3px rgba(0, 0, 0, 0.3), 0 1px rgba(255, 255, 255, 0.1);
+  transition: all 0.1s ease-in-out;
+  text-transform: uppercase;
+  font-weight: 800;
+  letter-spacing: 0.2em;
+}
+
+.toggle-switch label:hover {
+  cursor: pointer;
+}
+
+.toggle-switch input:checked + label {
+  background-color: #2a4365;
+  color: #ebf8ff;
+  box-shadow: none;
+}
+
+.toggle-switch label:first-of-type {
+  border-radius: 10px 0 0 10px;
+}
+
+.toggle-switch label:last-of-type {
+  border-radius: 0 10px 10px 0;
+}
+</style>
