f7773ef28e3eb49f4e027a74b5d4d3a505f66ff4
joselages <jose.lages.d12@gmail.com>
2020-08-07 18:38:16 +0100

    added checkbox to add current location on transition, get user lat and long with html location API, reverse geolocation with positionstack API

--- src/views/Create.vue
+++ src/views/Create.vue
@@ -1,68 +1,79 @@
 <template>
   <div class="w-full flex flex-col items-center" @keyup.enter="addTransaction">
     <h1>Add transaction</h1>
-    <div class="toggle-switch">
-      <input v-model="input.type" type="radio" id="debit" value="debit" name="transaction_type" />
-      <label for="debit">Debit</label>
+    <form class="border p-4 flex flex-col items-center mb-6">
+      <div class="toggle-switch">
+        <input v-model="input.type" type="radio" id="debit" value="debit" name="transaction_type" />
+        <label for="debit">Debit</label>
 
-      <input v-model="input.type" type="radio" id="credit" value="credit" name="transaction_type" />
-      <label for="credit">Credit</label>
-    </div>
+        <input v-model="input.type" type="radio" id="credit" value="credit" name="transaction_type" />
+        <label for="credit">Credit</label>
+      </div>
 
-    <div class="input-field">
-      <label for="description">Descrição</label>
-      <input
-        v-model="input.description"
-        type="text"
-        id="description"
-        name="transaction_description"
-        placeholder="Enter your description"
-      />
-    </div>
-
-    <div class="input-field">
-      <label for="hours">Horas</label>
-      <input v-model="input.hour" type="time" id="hours" name="transaction_hour" />
-    </div>
-
-    <div class="input-field">
-      <label for="date">Data</label>
-      <input v-model="input.date" type="date" id="date" name="transaction_date" />
-    </div>
-
-    <transition name="fadein">
-      <div
-        v-if="error.description"
-        class="bg-red-600 py-1 px-2 mb-2 txt-2 text-white text-xs rounded"
-      >{{error.description}}</div>
-    </transition>
-
-    <div class="input-field">
-      <label for="amount">Amount</label>
-      <div>
-        <span class="currency">$</span>
+      <div class="input-field">
+        <label for="description">Descrição</label>
         <input
-          @keydown="error.amount = ''"
-          v-model="input.amount"
+          v-model="input.description"
           type="text"
-          id="amount"
-          name="transaction_amount"
+          id="description"
+          name="transaction_description"
+          placeholder="Enter your description"
         />
       </div>
-    </div>
-    <transition name="fadein">
-      <div
-        class="bg-red-600 py-1 px-2 mb-2 txt-2 text-white text-xs rounded"
-        v-if="error.amount"
-      >{{error.amount}}</div>
-    </transition>
 
-    <div class="mt-4">
-      <button
-        class="border-2 border-purple-600 mb-6 px-16 py-2 rounded"
-        @click.prevent="addTransaction"
-      >Add</button>
-    </div>
+      <transition name="fadein">
+        <div
+          v-if="error.description"
+          class="bg-red-600 py-1 px-2 mb-2 txt-2 text-white text-xs rounded"
+        >{{error.description}}</div>
+      </transition>
+
+      <div class="input-field">
+        <label for="hours">Horas</label>
+        <input v-model="input.hour" type="time" id="hours" name="transaction_hour" />
+      </div>
+
+      <div class="input-field">
+        <label for="date">Data</label>
+        <input v-model="input.date" type="date" id="date" name="transaction_date" />
+      </div>
+
+      <div class="input-field">
+        <label for="amount">Amount</label>
+        <div>
+          <span class="currency">$</span>
+          <input
+            @keydown="error.amount = ''"
+            v-model="input.amount"
+            type="text"
+            id="amount"
+            name="transaction_amount"
+          />
+        </div>
+      </div>
+      <transition name="fadein">
+        <div
+          class="bg-red-600 py-1 px-2 mb-2 txt-2 text-white text-xs rounded"
+          v-if="error.amount"
+        >{{error.amount}}</div>
+      </transition>
+
+      <div class="ml-auto">
+        <label for="location">
+          <input @click="getLocation" type="checkbox" v-model="input.location" />
+          Add current location
+        </label>
+      </div>
+
+      <div class="mt-4">
+        <button
+          v-if="!loadingState"
+          class="border-2 border-purple-600 px-16 py-2 rounded"
+          @click.prevent="addTransaction"
+        >Add</button>
+        <img v-else src="../assets/loading.gif" />
+      </div>
+    </form>
   </div>
 </template>
 
@@ -76,7 +87,10 @@ export default {
         amount: "",
         date: null,
         hour: null,
+        location: false,
+        address: "",
       },
+      loadingState: false,
       error: {
         description: "",
         amount: "",
@@ -84,6 +98,37 @@ export default {
     };
   },
   methods: {
+    getLocation: function () {
+      if (!this.input.location) {
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(this.getCoordinates);
+        } else {
+          console.log("error");
+        }
+      }
+    },
+    getCoordinates: function (data) {
+      this.getAddress(data.coords.latitude, data.coords.longitude);
+    },
+    getAddress: function (lat, lon) {
+      var apikey = "bc1943561fb626f3a5b51febb8ec7117";
+      var self = this;
+      self.loadingState = true;
+
+      fetch(
+        "http://api.positionstack.com/v1/reverse?access_key=" +
+          apikey +
+          "&query=" +
+          lat +
+          "," +
+          lon
+      )
+        .then((response) => response.json())
+        .then(function (data) {
+          self.input.address = data.data[0];
+          self.loadingState = false;
+        });
+    },
     addTransaction: function () {
       if (this.validateForm() > 0) {
         //stops
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -21,10 +21,14 @@
             :class="transaction.type === 'credit' ? 'bg-green-400' : 'bg-red-400'"
             class="w-6 h-6 rounded-full flex justify-center items-center text-white font-bold text-xl"
           >{{transaction.type === "credit" ? "+" : "-"}}</div>
-          <div
-            :title="transaction.description"
-            class="ml-4 w-40 truncated_txt"
-          >{{transaction.description}}</div>
+          <div class="ml-4 w-40">
+            <h3 :title="transaction.description" class="truncated_txt">{{transaction.description}}</h3>
+            <address
+              v-if="transaction.address"
+              :title="transaction.address.label"
+              class="text-xs truncated_txt not-italic"
+            >@ {{transaction.address.label}}</address>
+          </div>
         </div>
         <div class="flex">
           <div class="mr-5">{{ formatMoney(transaction.amount) }}</div>
